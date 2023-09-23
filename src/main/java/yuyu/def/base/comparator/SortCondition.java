fe68e514b31e33c2f9a02e1f516b77b716be42ea
zhiqiang <y18525595720@gmail.com>
2023-09-23 23:38:26 +0800

    72364129

--- src/main/java/yuyu/def/base/comparator/SortCondition.java
+++ src/main/java/yuyu/def/base/comparator/SortCondition.java
@@ -1,14 +1,25 @@
 package yuyu.def.base.comparator;
 
 import java.lang.reflect.Method;
+
 import jp.co.slcs.swak.db.typesafe.ExDBEntity;
 import jp.co.slcs.swak.db.typesafe.ExDBField;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Comparator 検索条件保持クラス(タイプセーフ版)<br />
+ */
 public class SortCondition<E extends ExDBEntity<E, ?>> {
 
+    /**
+     * ソート順指定の列挙型です。
+     */
     public enum SortOrder {
+        ASC,
+        DESC
+    }
+
     @Getter @Setter
     private ExDBField<E, ?> sortKey;
 
