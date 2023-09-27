5872962fe505e6bbfe3cfc12b57ea8f2090f284c
zhiqiang <y18525595720@gmail.com>
2023-09-27 21:00:34 +0800

    782872489

--- src/main/java/yuyu/common/dsweb/dwcommon/DwMessageCodeConstants.java
+++ src/main/java/yuyu/common/dsweb/dwcommon/DwMessageCodeConstants.java
@@ -6,167 +6,89 @@ package yuyu.common.dsweb.dwcommon;
 public class DwMessageCodeConstants {
 
     public static final String IWSDSMSGCD_000000     = "000000";
-
     public static final String IWSDSMSGCD_001000     = "001000";
-
     public static final String IWSDSMSGCD_001001     = "001001";
-
     public static final String IWSDSMSGCD_001002     = "001002";
-
     public static final String IWSDSMSGCD_001003     = "001003";
-
     public static final String IWSDSMSGCD_001004     = "001004";
-
     public static final String IWSDSMSGCD_001005     = "001005";
-
     public static final String IWSDSMSGCD_001006     = "001006";
-
     public static final String IWSDSMSGCD_001007     = "001007";
-
     public static final String IWSDSMSGCD_001100     = "001100";
-
     public static final String IWSDSMSGCD_001200     = "001200";
-
     public static final String IWSDSMSGCD_001201     = "001201";
-
     public static final String IWSDSMSGCD_001300     = "001300";
-
     public static final String IWSDSMSGCD_001202     = "001202";
-
     public static final String IWSDSMSGCD_001203     = "001203";
-
     public static final String IWSDSMSGCD_001008     = "001008";
-
     public static final String IWSDSMSGCD_002001     = "002001";
-
     public static final String IWSDSMSGCD_002002     = "002002";
-
     public static final String IWSDSMSGCD_002003     = "002003";
-
     public static final String IWSDSMSGCD_002100     = "002100";
-
     public static final String IWSDSMSGCD_002200     = "002200";
-
     public static final String IWSDSMSGCD_002201     = "002201";
-
     public static final String IWSDSMSGCD_002202     = "002202";
-
     public static final String IWSDSMSGCD_002203     = "002203";
-
     public static final String IWSDSMSGCD_002204     = "002204";
-
     public static final String IWSDSMSGCD_002205     = "002205";
-
     public static final String IWSDSMSGCD_002206     = "002206";
-
     public static final String IWSDSMSGCD_003100     = "003100";
-
     public static final String IWSDSMSGCD_003200     = "003200";
-
     public static final String IWSDSMSGCD_003201     = "003201";
-
     public static final String IWSDSMSGCD_003202     = "003202";
-
     public static final String IWSDSMSGCD_003203     = "003203";
-
     public static final String IWSDSMSGCD_006100     = "006100";
-
     public static final String IWSDSMSGCD_006200     = "006200";
-
     public static final String IWSDSMSGCD_006201     = "006201";
-
     public static final String IWSDSMSGCD_006202     = "006202";
-
     public static final String IWSDSMSGCD_006203     = "006203";
-
     public static final String IWSDSMSGCD_006204     = "006204";
-
     public static final String IWSDSMSGCD_006205     = "006205";
-
     public static final String IWSDSMSGCD_007100     = "007100";
-
     public static final String IWSDSMSGCD_007200     = "007200";
-
     public static final String IWSDSMSGCD_007000     = "007000";
-
     public static final String IWSDSMSGCD_004100     = "004100";
-
     public static final String IWSDSMSGCD_004200     = "004200";
-
     public static final String IWSDSMSGCD_004201     = "004201";
-
     public static final String IWSDSMSGCD_004202     = "004202";
-
     public static final String IWSDSMSGCD_004203     = "004203";
-
     public static final String IWSDSMSGCD_004204     = "004204";
-
     public static final String IWSDSMSGCD_004001     = "004001";
-
     public static final String IWSDSMSGCD_004000     = "004000";
-
     public static final String IWSDSMSGCD_004002     = "004002";
-
     public static final String IWSDSMSGCD_005100     = "005100";
-
     public static final String IWSDSMSGCD_005200     = "005200";
-
     public static final String IWSDSMSGCD_005000     = "005000";
-
     public static final String IWSDSMSGCD_005201     = "005201";
-
     public static final String IWSDSMSGCD_005202     = "005202";
-
     public static final String IWSDSMSGCD_008000     = "008000";
-
     public static final String IWSDSMSGCD_009100     = "009100";
     public static final String IWSDSMSGCD_009200     = "009200";
     public static final String IWSDSMSGCD_009201     = "009201";
-
     public static final String IWSDSMSGCD_009202     = "009202";
-
     public static final String IWSDSMSGCD_009203     = "009203";
-
     public static final String IWSDSMSGCD_010212     = "010212";
-
     public static final String IWSDSMSGCD_010213     = "010213";
-
     public static final String IWSDSMSGCD_010100     = "010100";
-
     public static final String IWSDSMSGCD_010200     = "010200";
-
     public static final String IWSDSMSGCD_010214     = "010214";
-
     public static final String IWSDSMSGCD_010201     = "010201";
-
     public static final String IWSDSMSGCD_010202     = "010202";
-
     public static final String IWSDSMSGCD_010203     = "010203";
-
     public static final String IWSDSMSGCD_010204     = "010204";
-
     public static final String IWSDSMSGCD_010205     = "010205";
-
     public static final String IWSDSMSGCD_010206     = "010206";
-
     public static final String IWSDSMSGCD_010207     = "010207";
-
     public static final String IWSDSMSGCD_010208     = "010208";
-
     public static final String IWSDSMSGCD_010209     = "010209";
-
     public static final String IWSDSMSGCD_010210     = "010210";
-
     public static final String IWSDSMSGCD_010211     = "010211";
-
     public static final String IWSDSMSGCD_010000     = "010000";
-
     public static final String IWSDSMSGCD_015100     = "015100";
     public static final String IWSDSMSGCD_015200     = "015200";
     public static final String IWSDSMSGCD_015202     = "015202";
     public static final String IWSDSMSGCD_015203     = "015203";
     public static final String IWSDSMSGCD_015000     = "015000";
-
     public static final String IWSDSMSGCD_013014     = "013014";
     public static final String IWSDSMSGCD_013100     = "013100";
     public static final String IWSDSMSGCD_013001     = "013001";
@@ -206,7 +128,6 @@ public class DwMessageCodeConstants {
     public static final String IWSDSMSGCD_016202     = "016202";
     public static final String IWSDSMSGCD_016203     = "016203";
     public static final String IWSDSMSGCD_016000     = "016000";
-
     public static final String IWSDSMSGCD_012202     = "012202";
     public static final String IWSDSMSGCD_012203     = "012203";
     public static final String IWSDSMSGCD_012100     = "012100";
@@ -222,7 +143,6 @@ public class DwMessageCodeConstants {
     public static final String IWSDSMSGCD_012200     = "012200";
     public static final String IWSDSMSGCD_012201     = "012201";
     public static final String IWSDSMSGCD_012300     = "012300";
-
     public static final String IWSDSMSGCD_014205     = "014205";
     public static final String IWSDSMSGCD_014204     = "014204";
     public static final String IWSDSMSGCD_014100     = "014100";
@@ -234,49 +154,28 @@ public class DwMessageCodeConstants {
     public static final String IWSDSMSGCD_014202     = "014202";
     public static final String IWSDSMSGCD_014002     = "014002";
     public static final String IWSDSMSGCD_014000     = "014000";
-
-
     public static final String IWSDSMSGCD_011000     = "011000";
-
     public static final String IWSDSMSGCD_011100     = "011100";
-
     public static final String IWSDSMSGCD_011200     = "011200";
-
     public static final String IWSDSMSGCD_011201     = "011201";
-
     public static final String IWSDSMSGCD_011202     = "011202";
-
     public static final String IWSDSMSGCD_011203     = "011203";
-
     public static final String IWSDSMSGCD_011204     = "011204";
-
     public static final String IWSDSMSGCD_011205     = "011205";
-
     public static final String IWSDSMSGCD_011206     = "011206";
-
     public static final String IWSDSMSGCD_011207     = "011207";
-
     public static final String IWSDSMSGCD_011208     = "011208";
-
     public static final String IWSDSMSGCD_011209     = "011209";
-
     public static final String IWSDSMSGCD_011210     = "011210";
-
     public static final String IWSDSMSGCD_011211     = "011211";
-
     public static final String IWSDSMSGCD_011212     = "011212";
-
     public static final String IWSDSMSGCD_011213     = "011213";
-
     public static final String IWSDSMSGCD_011214     = "011214";
-
     public static final String IWSDSMSGCD_011215     = "011215";
-
     public static final String IWSDSMSGCD_018000     = "018000";
     public static final String IWSDSMSGCD_018100     = "018100";
     public static final String IWSDSMSGCD_018202     = "018202";
     public static final String IWSDSMSGCD_018203     = "018203";
-
     public static final String IWSDSMSGCD_017000     = "017000";
     public static final String IWSDSMSGCD_017100     = "017100";
     public static final String IWSDSMSGCD_017200     = "017200";
@@ -296,82 +195,42 @@ public class DwMessageCodeConstants {
     public static final String IWSDSMSGCD_017214     = "017214";
     public static final String IWSDSMSGCD_017216     = "017216";
     public static final String IWSDSMSGCD_017217     = "017217";
-
-
     public static final String IWSDSMSGCD_019000     = "019000";
-
     public static final String IWSDSMSGCD_019100     = "019100";
-
     public static final String IWSDSMSGCD_019200     = "019200";
-
     public static final String IWSDSMSGCD_019201     = "019201";
-
     public static final String IWSDSMSGCD_019202     = "019202";
-
     public static final String IWSDSMSGCD_019203     = "019203";
-
     public static final String IWSDSMSGCD_019204     = "019204";
-
     public static final String IWSDSMSGCD_019205     = "019205";
-
     public static final String IWSDSMSGCD_019206     = "019206";
-
     public static final String IWSDSMSGCD_019207     = "019207";
-
     public static final String IWSDSMSGCD_019208     = "019208";
-
     public static final String IWSDSMSGCD_019209     = "019209";
-
     public static final String IWSDSMSGCD_019210     = "019210";
-
     public static final String IWSDSMSGCD_019211     = "019211";
-
     public static final String IWSDSMSGCD_019212     = "019212";
-
-
     public static final String IWSDSMSGCD_020000     = "020000";
-
     public static final String IWSDSMSGCD_020100     = "020100";
-
     public static final String IWSDSMSGCD_020200     = "020200";
-
     public static final String IWSDSMSGCD_020201     = "020201";
-
-
     public static final String IWSDSMSGCD_021000     = "021000";
-
     public static final String IWSDSMSGCD_021100     = "021100";
-
     public static final String IWSDSMSGCD_021101     = "021101";
-
     public static final String IWSDSMSGCD_021102     = "021102";
-
     public static final String IWSDSMSGCD_021103     = "021103";
-
     public static final String IWSDSMSGCD_021200     = "021200";
-
     public static final String IWSDSMSGCD_021201     = "021201";
-
     public static final String IWSDSMSGCD_021202     = "021202";
-
     public static final String IWSDSMSGCD_021203     = "021203";
-
     public static final String IWSDSMSGCD_021300     = "021300";
-
     public static final String IWSDSMSGCD_021301     = "021301";
-
     public static final String IWSDSMSGCD_021302     = "021302";
-
     public static final String IWSDSMSGCD_021303     = "021303";
-
     public static final String IWSDSMSGCD_021304     = "021304";
-
     public static final String IWSDSMSGCD_021305     = "021305";
-
     public static final String IWSDSMSGCD_021306     = "021306";
-
     public static final String IWSDSMSGCD_021307     = "021307";
-
     public static final String IWSDSMSGCD_022210     = "022210";
     public static final String IWSDSMSGCD_022209     = "022209";
     public static final String IWSDSMSGCD_022000     = "022000";
